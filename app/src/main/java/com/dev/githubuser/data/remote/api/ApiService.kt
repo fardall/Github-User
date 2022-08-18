0fec011c62aeebac2f8803396d78cc629a00006f
Fardal <aafardal8@gmail.com>
2022-08-18 09:23:09 +0700

    - Some stuff

--- app/src/main/java/com/dev/githubuser/data/remote/api/ApiService.kt
+++ app/src/main/java/com/dev/githubuser/data/remote/api/ApiService.kt
@@ -1,8 +1,6 @@
 package com.dev.githubuser.data.remote.api
 
-import com.dev.githubuser.data.remote.responses.DetailUserResponse
-import com.dev.githubuser.data.remote.responses.UserResponse
-import com.dev.githubuser.data.remote.responses.UsersResponse
+import com.dev.githubuser.data.remote.responses.*
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -10,25 +8,25 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("search/users")
     fun findUsers(
         @Query("q") q: String
     ): Call<UsersResponse>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("users/{username}")
     fun getUser(
         @Path("username") username: String?
     ): Call<DetailUserResponse>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("/users/{username}/followers")
     fun getFollowers(
         @Path("username") username: String
     ): Call<List<UserResponse>>
 
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     @GET("/users/{username}/following")
     fun getFollowing(
         @Path("username") username: String
